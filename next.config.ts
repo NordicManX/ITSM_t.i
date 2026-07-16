068ea1fb97aca9a46450fe257d84752b12efe00a
Nelson Carvalho <NordicManX@users.noreply.github.com>
2026-07-16 09:55:36 -0300

    ajuste de vazamentos de dados via http no arquivo next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,34 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // 1. Remove o vazamento de tecnologia
+  poweredByHeader: false, 
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  // 2. Injeta os escudos de segurança em todas as rotas
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY', // Proíbe que o sistema seja aberto em iframes de outros sites
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff', // Impede injeção de scripts disfarçados
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin', // Protege dados de navegação na URL
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains', // Força o uso estrito de HTTPS
+          },
+        ],
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+export default nextConfig; // ou module.exports = nextConfig; se for .js
